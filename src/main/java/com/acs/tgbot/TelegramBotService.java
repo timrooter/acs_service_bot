8776d5a232ade81fd3cb53bc3d245269ec18a3fd
Timur <anvarov360@gmail.com>
2024-06-14 17:26:57 +0500

    java 8

--- src/main/java/com/acs/tgbot/TelegramBotService.java
+++ src/main/java/com/acs/tgbot/TelegramBotService.java
@@ -1,6 +1,6 @@
 package com.acs.tgbot;
 
-import jakarta.annotation.PostConstruct;
+import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
